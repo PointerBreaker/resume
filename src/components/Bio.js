a43f2b4001fd107d4e4536b14ad2f815d58b9bd3
Jero <Jero@DESKTOP-LT4KBI9>
2021-02-01 20:14:26 +0100

    Added Linked in and profile picture. updated styling

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,19 +4,21 @@ const styles = StyleSheet.create({
     text: { fontSize: 12, width: 160},
     view: { alignItems: "flex-start", display: "flex", flexDirection: "row", justifyContent: "space-between", width: 175},
     image: { width: 12, height: 12, },
+    viewColumn: { display: "flex", flexDirection: "column", justifyContent: "space-between"},
   });
 
 export default function Bio() {
     
     return (
-           <>
+           <View style={styles.viewColumn}>
             <BioItem src={"/images/name.png"} text={"Jeroen Kooiman"}/>
             <BioItem src={"/images/home.png"} text={"Sliedrecht, Zuid-Holland"}/>
             <BioItem src={"/images/cake.png"} text={"19 Feb 1993"}/>
             <BioItem src={"/images/email.png"} text={"jeroen@jeroenkooiman.net"} url={"mailto:jeroen@jeroenkooiman.net"}/>
-            <BioItem src={"/images/phone.png"} text={"06 45 65 27 54"} url={"tel:+31645652754"}/>
+            <BioItem src={"/images/phone.png"} text={"06 45 65 27 54"} />
             <BioItem src={"/images/globe.png"} text={"jeroenkooiman.net"} url={"https://jeroenkooiman.net"}/>
-          </>
+            <BioItem src={"/images/linkedin.png"} text="linkedin.com/in/jpkooiman/" url={"https://www.linkedin.com/in/jpkooiman/"}></BioItem>
+          </View>
     );
 }
 
--- src/components/PDFDocument.js
+++ src/components/PDFDocument.js
@@ -1,17 +1,27 @@
-import { Document, Page, View, Text, StyleSheet} from "@react-pdf/renderer";
+import { Document, Page, Image, View, Text, StyleSheet} from "@react-pdf/renderer";
 import Skills from "./SkillSection";
 import Bio from "./Bio";
+
+const styles = StyleSheet.create({
+  bioView: { marginTop: 6, alignSelf: "flex-end", display: "flex", flexDirection: "row", justifyContent: "space-between", width: 280},
+  skills: { marginTop: 5, alignSelf: "flex-end", borderTop: "1 solid black" }
+});
 
 export default function PDF() {
   return (
     <Document author="Jeroen Kooiman">
-      <Page size="A4">    
-          <Bio />
-          {/* <Summary /> */}
+      <Page size="A4"> 
+        <View style={styles.bioView}>
+            <Image style={{width: 100, height: 100}}src="/images/pfp.jpg"/>
+            <Bio />
+        </View>
+          {/* <Summary /> */}  
           {/* <Experience /> */}
           {/* <Education /> */}        
           {/* <Other /> */}
-          <Skills />
+          <View style={styles.skills}>
+            <Skills />
+          </View>
       </Page>
     </Document>
   );
